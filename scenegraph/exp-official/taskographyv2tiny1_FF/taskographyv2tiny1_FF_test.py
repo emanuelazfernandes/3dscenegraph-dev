18c8c9e6f84375f00507b38617e481dc36ddef75
emanuel.a.fernandes <emanuel.a.fernandes@ist.utl.pt>
2022-11-04 17:29:37 +0100

    fixing mystery disappearances

--- scenegraph/exp-official/taskographyv2tiny1_FF/taskographyv2tiny1_FF_test.py
+++ scenegraph/exp-official/taskographyv2tiny1_FF/taskographyv2tiny1_FF_test.py
@@ -2,329 +2,329 @@ STATS = [
     {
         "num_node_expansions": 19,
         "plan_length": 16,
-        "search_time": 0.18,
-        "total_time": 0.18
+        "search_time": 0.19,
+        "total_time": 0.19
     },
     {
-        "num_node_expansions": 22,
+        "num_node_expansions": 41,
         "plan_length": 18,
-        "search_time": 0.22,
-        "total_time": 0.22
+        "search_time": 0.21,
+        "total_time": 0.21
     },
     {
-        "num_node_expansions": 16,
+        "num_node_expansions": 57,
         "plan_length": 13,
         "search_time": 0.17,
         "total_time": 0.17
     },
     {
-        "num_node_expansions": 33,
+        "num_node_expansions": 90,
         "plan_length": 26,
-        "search_time": 0.26,
-        "total_time": 0.26
+        "search_time": 0.27,
+        "total_time": 0.27
     },
     {
-        "num_node_expansions": 23,
+        "num_node_expansions": 113,
         "plan_length": 18,
         "search_time": 0.24,
         "total_time": 0.24
     },
     {
-        "num_node_expansions": 24,
+        "num_node_expansions": 137,
         "plan_length": 21,
         "search_time": 0.11,
         "total_time": 0.11
     },
     {
-        "num_node_expansions": 21,
-        "plan_length": 16,
-        "search_time": 0.12,
-        "total_time": 0.12
-    },
-    {
-        "num_node_expansions": 22,
-        "plan_length": 17,
-        "search_time": 0.1,
-        "total_time": 0.1
-    },
-    {
-        "num_node_expansions": 19,
+        "num_node_expansions": 158,
         "plan_length": 16,
         "search_time": 0.11,
         "total_time": 0.11
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 180,
+        "plan_length": 17,
+        "search_time": 0.1,
+        "total_time": 0.1
+    },
+    {
+        "num_node_expansions": 199,
+        "plan_length": 16,
+        "search_time": 0.1,
+        "total_time": 0.1
+    },
+    {
+        "num_node_expansions": 216,
         "plan_length": 13,
-        "search_time": 0.09,
-        "total_time": 0.09
+        "search_time": 0.1,
+        "total_time": 0.1
     },
     {
-        "num_node_expansions": 19,
+        "num_node_expansions": 235,
         "plan_length": 15,
-        "search_time": 0.21,
-        "total_time": 0.21
-    },
-    {
-        "num_node_expansions": 25,
-        "plan_length": 23,
-        "search_time": 0.28,
-        "total_time": 0.28
-    },
-    {
-        "num_node_expansions": 24,
-        "plan_length": 19,
-        "search_time": 0.23,
-        "total_time": 0.23
-    },
-    {
-        "num_node_expansions": 14,
-        "plan_length": 12,
         "search_time": 0.19,
         "total_time": 0.19
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 260,
+        "plan_length": 23,
+        "search_time": 0.25,
+        "total_time": 0.25
+    },
+    {
+        "num_node_expansions": 284,
+        "plan_length": 19,
+        "search_time": 0.24,
+        "total_time": 0.24
+    },
+    {
+        "num_node_expansions": 298,
+        "plan_length": 12,
+        "search_time": 0.18,
+        "total_time": 0.18
+    },
+    {
+        "num_node_expansions": 315,
         "plan_length": 12,
         "search_time": 0.2,
         "total_time": 0.2
     },
     {
-        "num_node_expansions": 16,
-        "plan_length": 14,
-        "search_time": 0.23,
-        "total_time": 0.23
-    },
-    {
-        "num_node_expansions": 12,
-        "plan_length": 9,
-        "search_time": 0.22,
-        "total_time": 0.22
-    },
-    {
-        "num_node_expansions": 23,
-        "plan_length": 18,
-        "search_time": 0.24,
-        "total_time": 0.24
-    },
-    {
-        "num_node_expansions": 18,
+        "num_node_expansions": 331,
         "plan_length": 14,
         "search_time": 0.21,
         "total_time": 0.21
     },
     {
-        "num_node_expansions": 22,
+        "num_node_expansions": 343,
+        "plan_length": 9,
+        "search_time": 0.19,
+        "total_time": 0.19
+    },
+    {
+        "num_node_expansions": 366,
+        "plan_length": 18,
+        "search_time": 0.23,
+        "total_time": 0.23
+    },
+    {
+        "num_node_expansions": 384,
+        "plan_length": 14,
+        "search_time": 0.19,
+        "total_time": 0.19
+    },
+    {
+        "num_node_expansions": 406,
         "plan_length": 17,
         "search_time": 0.23,
         "total_time": 0.23
     },
     {
-        "num_node_expansions": 26,
-        "plan_length": 23,
-        "search_time": 0.08,
-        "total_time": 0.08
-    },
-    {
-        "num_node_expansions": 22,
-        "plan_length": 19,
-        "search_time": 0.08,
-        "total_time": 0.08
-    },
-    {
-        "num_node_expansions": 15,
-        "plan_length": 12,
-        "search_time": 0.07,
-        "total_time": 0.07
-    },
-    {
-        "num_node_expansions": 25,
-        "plan_length": 20,
-        "search_time": 0.08,
-        "total_time": 0.08
-    },
-    {
-        "num_node_expansions": 26,
+        "num_node_expansions": 432,
         "plan_length": 23,
         "search_time": 0.09,
         "total_time": 0.09
     },
     {
-        "num_node_expansions": 43,
+        "num_node_expansions": 454,
+        "plan_length": 19,
+        "search_time": 0.08,
+        "total_time": 0.08
+    },
+    {
+        "num_node_expansions": 469,
+        "plan_length": 12,
+        "search_time": 0.08,
+        "total_time": 0.08
+    },
+    {
+        "num_node_expansions": 494,
+        "plan_length": 20,
+        "search_time": 0.08,
+        "total_time": 0.08
+    },
+    {
+        "num_node_expansions": 520,
+        "plan_length": 23,
+        "search_time": 0.09,
+        "total_time": 0.09
+    },
+    {
+        "num_node_expansions": 563,
         "plan_length": 40,
         "search_time": 1.15,
         "total_time": 1.15
     },
     {
-        "num_node_expansions": 24,
+        "num_node_expansions": 587,
         "plan_length": 20,
-        "search_time": 0.55,
-        "total_time": 0.55
+        "search_time": 0.56,
+        "total_time": 0.56
     },
     {
-        "num_node_expansions": 21,
+        "num_node_expansions": 608,
         "plan_length": 15,
-        "search_time": 0.47,
-        "total_time": 0.47
+        "search_time": 0.49,
+        "total_time": 0.49
     },
     {
-        "num_node_expansions": 37,
+        "num_node_expansions": 645,
         "plan_length": 28,
-        "search_time": 0.72,
-        "total_time": 0.72
+        "search_time": 0.73,
+        "total_time": 0.73
     },
     {
-        "num_node_expansions": 35,
+        "num_node_expansions": 680,
         "plan_length": 29,
-        "search_time": 0.72,
-        "total_time": 0.72
+        "search_time": 0.73,
+        "total_time": 0.73
     },
     {
-        "num_node_expansions": 29,
+        "num_node_expansions": 709,
         "plan_length": 27,
-        "search_time": 0.51,
-        "total_time": 0.51
+        "search_time": 0.53,
+        "total_time": 0.53
     },
     {
-        "num_node_expansions": 27,
+        "num_node_expansions": 736,
         "plan_length": 22,
-        "search_time": 0.48,
-        "total_time": 0.48
+        "search_time": 0.46,
+        "total_time": 0.46
     },
     {
-        "num_node_expansions": 11,
+        "num_node_expansions": 747,
         "plan_length": 9,
         "search_time": 0.28,
         "total_time": 0.28
     },
     {
-        "num_node_expansions": 19,
+        "num_node_expansions": 766,
         "plan_length": 16,
-        "search_time": 0.36,
-        "total_time": 0.36
+        "search_time": 0.35,
+        "total_time": 0.35
     },
     {
-        "num_node_expansions": 20,
+        "num_node_expansions": 786,
         "plan_length": 18,
         "search_time": 0.41,
         "total_time": 0.41
     },
     {
-        "num_node_expansions": 13,
+        "num_node_expansions": 799,
         "plan_length": 10,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 15,
+        "num_node_expansions": 814,
         "plan_length": 13,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 18,
+        "num_node_expansions": 832,
         "plan_length": 16,
-        "search_time": 0.03,
-        "total_time": 0.03
+        "search_time": 0.04,
+        "total_time": 0.04
     },
     {
-        "num_node_expansions": 15,
+        "num_node_expansions": 847,
         "plan_length": 11,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 864,
         "plan_length": 13,
-        "search_time": 0.04,
-        "total_time": 0.04
+        "search_time": 0.03,
+        "total_time": 0.03
     },
     {
-        "num_node_expansions": 16,
+        "num_node_expansions": 880,
         "plan_length": 12,
         "search_time": 0.02,
         "total_time": 0.02
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 897,
         "plan_length": 14,
-        "search_time": 0.02,
-        "total_time": 0.02
+        "search_time": 0.01,
+        "total_time": 0.01
     },
     {
-        "num_node_expansions": 20,
+        "num_node_expansions": 917,
         "plan_length": 15,
         "search_time": 0.02,
         "total_time": 0.02
     },
     {
-        "num_node_expansions": 15,
+        "num_node_expansions": 932,
         "plan_length": 11,
         "search_time": 0.02,
         "total_time": 0.02
     },
     {
-        "num_node_expansions": 26,
+        "num_node_expansions": 958,
         "plan_length": 20,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 19,
+        "num_node_expansions": 977,
         "plan_length": 15,
         "search_time": 0.12,
         "total_time": 0.12
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 994,
         "plan_length": 15,
         "search_time": 0.11,
         "total_time": 0.11
     },
     {
-        "num_node_expansions": 10,
+        "num_node_expansions": 1004,
         "plan_length": 8,
-        "search_time": 0.1,
-        "total_time": 0.1
+        "search_time": 0.11,
+        "total_time": 0.11
     },
     {
-        "num_node_expansions": 15,
+        "num_node_expansions": 1019,
         "plan_length": 11,
         "search_time": 0.11,
         "total_time": 0.11
     },
     {
-        "num_node_expansions": 27,
+        "num_node_expansions": 1046,
         "plan_length": 22,
-        "search_time": 0.14,
-        "total_time": 0.14
+        "search_time": 0.13,
+        "total_time": 0.13
     },
     {
-        "num_node_expansions": 19,
+        "num_node_expansions": 1065,
         "plan_length": 14,
-        "search_time": 0.03,
-        "total_time": 0.03
+        "search_time": 0.02,
+        "total_time": 0.02
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 1082,
         "plan_length": 15,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 17,
+        "num_node_expansions": 1099,
         "plan_length": 14,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 12,
+        "num_node_expansions": 1111,
         "plan_length": 9,
         "search_time": 0.03,
         "total_time": 0.03
     },
     {
-        "num_node_expansions": 15,
+        "num_node_expansions": 1126,
         "plan_length": 13,
         "search_time": 0.03,
         "total_time": 0.03
--- scenegraph/pddlgym_planners/ff.py
+++ scenegraph/pddlgym_planners/ff.py
@@ -30,6 +30,7 @@ class FF(PDDLPlanner):
         return cmd_str
 
     def _get_cmd_str_searchonly(self, sas_file, timeout):
+        
         raise NotImplementedError("Cannot run translate_separately=True for FF")
 
     def _output_to_plan(self, output):
@@ -40,8 +41,9 @@ class FF(PDDLPlanner):
             self._statistics["num_node_expansions"] = 0
         if len(num_node_expansions) == 1:
             assert int(num_node_expansions[0]) == float(num_node_expansions[0])
-            self._statistics["num_node_expansions"] += int(
-                num_node_expansions[0])
+            # self._statistics["num_node_expansions"] += int(
+            #     num_node_expansions[0])
+            self._statistics["num_node_expansions"] = int(num_node_expansions[0])
         if "found legal plan" in output:
             plan_length = re.findall(r"(\d+):", output.lower())
             self._statistics["plan_length"] = len(plan_length)
@@ -79,7 +81,7 @@ class FF(PDDLPlanner):
         else:
             # Install FF directly from official website.
             os.system("curl {} --output {}.tgz".format(FF_URL, loc))
-            os.system("tar -xzvf {}.tgz".format(loc))
+            os.system("tar -xzvf {}.tgz -C {}".format(loc, os.path.dirname(loc)))
         # Compile FF.
         os.system("cd {} && make && cd -".format(loc))
         assert os.path.exists(self._exec)
